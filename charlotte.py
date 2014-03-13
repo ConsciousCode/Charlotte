6cd9c54d8c0f3c147d5264082c7cb97224d94314
ConsciousCode <conscious.code@gmail.com>
2014-03-13 16:17:07 -0600

    Made a common error function, fixed a bug in order preservation

--- charlotte.py
+++ charlotte.py
@@ -5,6 +5,9 @@ import random
 import time
 import pickle
 
+def error(msg,e):
+	open("error.log","a").write("{}: {}\n\t{}".format(time.asctime(),msg,'\t'.join(traceback.format_exception(type(e),e,e.__traceback__))))
+
 #Windows
 if sys.platform.startswith("win"):
 	import ctypes
@@ -48,7 +51,9 @@ def traverse(p):
 	wallpapers=list()
 	
 	for path,dirs,files in os.walk(p):
-		wallpapers.extend([os.path.join(path,f) for f in files if os.path.splitext(f)[1] in imgexts])
+		#narrow down the list so only images remain
+		wallpapers.extend([os.path.join(path,f) for f in files if os.path.splitext(f)[1].lower() in imgexts])
+	
 	return wallpapers
 
 #load the pickled status from the status file
@@ -61,14 +66,15 @@ def read_status():
 				i+=1
 			else:
 				#file's missing, produce new indices
+				#status file will be created later
 				indices=[x for x in range(len(wallpapers))]
 				random.shuffle(indices)
 				i=0
 			break
-		except (IOError,EOFError,pickle.PickleError):
+		except (IOError,EOFError,pickle.PickleError) as e:
 			os.remove("status")
 			
-			open("error.log","a").write("{}: Failed to load status dump\n\t{}".format(time.asctime(),'\t'.join(traceback.format_exception(type(e),e,e.__traceback__))))
+			error("Failed to load status dump",e)
 	
 	return indices,i
 
@@ -77,29 +83,33 @@ def write_status(indices,i):
 	try:
 		pickle.dump((indices,i),open("status","wb"))
 	except IOError as e:
-		open("error.log","a").write("{}: Failed to write status dump\n\t{}".format(time.asctime(),'\t'.join(traceback.format_exception(type(e),e,e.__traceback__))))
+		error("Failed to write status dump",e)
 
 #update the indices and shuffling status file given the
 #wallpaper and check lengths
 def update(i,ind,wall,check):
 	num=wall-check
+	#files have been added
 	if num>0:
 		for x in range(wall-num,wall):
 			v=indices.index(x)
 			#adjust the index so modification doesn't
 			#affect iteration
 			if v<i:
-				i+=1
+				i-=1
 			del indices[v]
+	#files have been removed
 	elif num<0:
 		for x in range(check,check-num):
 			v=random.randrange(i,wall)
 			
 			if v<=i:
-				i-=1
+				i+=1
 			indices.insert(v,x)
 	
 	write_status(indices,i)
+	
+	return i
 
 wallpapers=traverse(root)
 
@@ -108,44 +118,49 @@ indices,i=read_status()
 
 #indefinitely loop
 while True:
-	#iterate over the indices
-	#(the indices are shuffled rather than the wallpapers
-	# to support adding and removing wallpapers to the
-	# directory without complicated insertion and iteration
-	# algorithms)
-	while i<len(indices):
-		while True:
-			try:
-				wall=wallpapers[indices[i]]
-				#check for the edge case that the user
-				#removed the file since the last traversal
-				if not os.path.isfile(wall):
-					raise IndexError()
-				set_wallpaper(wall)
-				break
-			except IndexError:
-				check=traverse(root)
-				update(i,indices,len(wallpapers),len(check))
-				wallpapers=check
+	#catch exceptions for logging before quitting
+	try:
+		#iterate over the indices
+		#(the indices are shuffled rather than the wallpapers
+		# to support adding and removing wallpapers to the
+		# directory without complicated insertion and iteration
+		# algorithms)
+		while i<len(indices):
+			while True:
+				try:
+					wall=wallpapers[indices[i]]
+					#check for the edge case that the user
+					#removed the file since the last traversal
+					if not os.path.isfile(wall):
+						#essentially using as a goto
+						raise IndexError()
+					set_wallpaper(wall)
+					break
+				except IndexError:
+					check=traverse(root)
+					i=update(i,indices,len(wallpapers),len(check))
+					wallpapers=check
+			
+			#start a timer just in case the costly operations
+			#take too long
+			start=time.perf_counter()
+			
+			#check for any new wallpapers
+			check=traverse(root)
+			
+			#update the indices and shuffling status
+			i=update(i,indices,len(wallpapers),len(check))
+			wallpapers=check
+			
+			i+=1
+			#sleep by the delay minus however much time has
+			#already passed
+			time.sleep(delay+start-time.perf_counter())
 		
-		#start a timer just in case the costly operations
-		#take too long
-		start=time.perf_counter()
-		
-		#check for any new wallpapers
-		check=traverse(root)
-		
-		#update the indices and shuffling status
-		update(i,indices,len(wallpapers),len(check))
-		wallpapers=check
-		
-		i+=1
-		#sleep by the delay minus however much time has
-		#already passed
-		time.sleep(delay+start-time.perf_counter())
-	
-	i=0
-	#shuffle the indices instead of the wallpapers
-	random.shuffle(indices)
-	#dump the fresh status
-	write_status(indices,0)
+		i=0
+		#shuffle the indices instead of the wallpapers
+		random.shuffle(indices)
+		#dump the fresh status
+		write_status(indices,0)
+	except Exception as e:
+		error("I just don't know what went wrong",e)
